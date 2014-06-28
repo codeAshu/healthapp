d0e940841ac98dee66e183263252df622f2f4dbf
codeAshu <rusty.iceberg@gmail.com>
2014-06-28 12:57:06 +0530

    dashboard

--- healthApp/src/com/iiitb/healthapp/DashboardActivity.java
+++ healthApp/src/com/iiitb/healthapp/DashboardActivity.java
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
- 
-
 public class DashboardActivity extends Activity {
 	UserFunctions userFunctions;
 	Button btnLogout;
